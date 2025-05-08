5a58712c5c23b49358a3c22266c7ca72f224160f
Cristian_Recabarren <109612110+bit-rec98@users.noreply.github.com>
2025-05-08 15:59:11 -0300

    Added environments

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  // apiUrl: 'http://localhost:3000/api',
+  // imageUrl: 'http://localhost:3000/assets',
+  appName: 'dot.code [DEV]',
+  version: '1.0.0-dev',
+};
